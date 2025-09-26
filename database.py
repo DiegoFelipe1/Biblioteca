8ea6a237a171c0784dfe9bf372bc6e33b1af3062
Diego Felipe <fdiego039@gmail.com>
2025-09-26 18:35:18 -0300

    incluindo campo de email e senha

--- database.py
+++ database.py
@@ -15,7 +15,9 @@ class Usuario(Base):
     __tablename__ = "Usuarios"
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String, nullable=False)
-    contato = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    senha = Column(String, nullable=False)
+    role = Column(String, default="user")
 
     #Relação de Usuario com emprestimo
     emprestimos = relationship("Emprestimo", back_populates="usuario")
