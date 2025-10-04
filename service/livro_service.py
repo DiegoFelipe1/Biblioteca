46d2ed45a3e5e45d687f551a10b2511eff32567a
Diego Felipe <fdiego039@gmail.com>
2025-10-03 21:09:39 -0300

    Novos serviços que livro pode realizar

--- service/livro_service.py
+++ service/livro_service.py
@@ -3,23 +3,28 @@ from models.usuario import Usuario
 from models.emprestimo import Emprestimo
 from database import SessionLocal
 
-def listar_livros():
+def listar_livros() -> list:
     with SessionLocal() as session:
         livros = session.query(Livro).all()
+        
         if not livros:
-            return False
-        else:
-            print("\n📚 Livros disponiveis:\n")
-            for livro in livros:
-                print(f"{livro.id}. {livro.titulo} || {livro.autor} || {livro.ano}")
-
-def adicionar_livro():
-    titulo = input("\nDigite o titulo: ")
-    autor = input("\nDigite o nome do autor: ")
-    ano = int(input("\nDigite o ano do livro: "))
-
+            return []
+            
+        return livros
+        
+def adicionar_livro(titulo: str, autor: str, ano: int) -> bool:
     with SessionLocal() as session:
         try:
+            if not titulo:
+                print("Por favor digite um titulo")
+                return False
+            if not autor:
+                print("Por favor, digite um autor")
+                return False
+            if not ano:
+                print("Por favor digite um ano.")
+                return False
+            
             novo_livro = Livro(
                 titulo=titulo,
                 ano=ano, 
@@ -29,10 +34,12 @@ def adicionar_livro():
             session.commit()
 
             print(f"✅ O livro {titulo} foi adicionado com sucesso")
-
+            return True
+        
         except Exception as e:
             session.rollback()
             print("Erro ao Registrar:", e)
+            return False
 
 def historico_livros(id_usuario: str) -> bool:
     with SessionLocal() as session:
@@ -49,5 +56,41 @@ def historico_livros(id_usuario: str) -> bool:
                     return True
 
             except Exception as e:
+                session.rollback()
                 print(f"Falha em listar livros empretado: {e}")
                 return False
+
+def editar_livro(id: int, titulo: str, autor: str, ano: int) -> bool:
+    with SessionLocal() as session:
+        try:
+            livro = session.query(Livro).filter(Livro.id == id).first()
+
+            livro.titulo = titulo
+            livro.autor = autor
+            livro.ano = ano
+
+            session.commit()
+
+            return True
+        
+        except Exception as e:
+            session.rollback()
+            print(f"Falha na edição: {e}")
+            return False
+
+def excluir_livro(id: int) -> bool:
+    with SessionLocal() as session:
+        try:
+            livro = session.query(Livro).filter(Livro.id == id).first()
+
+            if not livro:
+                print("Nenhum livro corresponde a esse id.")
+
+            session.delete(livro)
+            session.commit()
+            return True
+        
+        except Exception as e:
+            session.rollback()
+            print(f"Erro ao excluir: {e}")
+            return False
